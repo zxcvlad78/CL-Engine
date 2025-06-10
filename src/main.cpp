35f095137d5adff82a9f7f16304fa17111a59f77
zxcvlad78 <143466931+zxcvlad78@users.noreply.github.com>
2025-06-10 08:50:06 +0300

    RENDER

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,59 @@
+#include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 int main(void)
 {
-    GLFWwindow* window;
+    short WIDTH = 640;
+    short HEIGTH = WIDTH / 16 * 9;    
 
-    /* Initialize the library */
+    char NAME[] = "CL Engine";
+
+
     if (!glfwInit())
+    {
+        std::cout << "glfwInit failed" << std::endl;
         return -1;
+    }
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window;
+    window = glfwCreateWindow(WIDTH, HEIGTH, NAME, NULL, NULL);
     if (!window)
     {
+        std::cout << "glfwCreateWindow" << std::endl;
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    if (!gladLoadGL()) {
+        std::cout << "gladLoadGL failed" << std::endl;
+        return -1;
+    }
+
+    std::cout << "OpenGL version: " << GLVersion.major << '.' << GLVersion.minor << std::endl;
+
+
+
+
+    //std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    //std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl; 
+
+    glClearColor(1,0,0,1);
+
+    // Game Loop
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        // Render here
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
